d2a74489c42ef8112401906a17b744dea415f425
savwilliams <wil24021@byui.edu>
2025-03-18 09:14:07 -0600

    creates alertss and info pages

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -43,3 +43,23 @@ export function footerTemplate(info) {
     </section>`;
 }
   
+
+export function alertTemplate(alert) {
+  let alertType = "";
+  switch (alert.category) {
+    case "Park Closure":
+      alertType = "closure";
+      break;
+    default:
+      alertType = alert.category.toLowerCase();
+  }  
+ 
+  return `<li class="alert">
+  <svg class="icon" focusable="false" aria-hidden="true">
+    <use xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use>
+  </svg>
+  <div>
+    <h3 class="alert-${alertType}">${alert.title}</h3>
+    <p>${alert.description}</p>
+  </div></li>`;
+}
